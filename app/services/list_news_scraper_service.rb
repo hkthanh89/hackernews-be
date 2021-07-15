bf69d04cbe4e5346baa7d0d1536acbeefb48838d
Thanh Huynh <hkthanh89@users.noreply.github.com>
2021-07-15 23:08:04 +0700

    caching list news and news detail

--- app/services/list_news_scraper_service.rb
+++ app/services/list_news_scraper_service.rb
@@ -6,46 +6,49 @@ class ListNewsScraperService
 
   def self.execute(page: )
     page = page.to_i > 0 ? page.to_i : 1
+    url = "https://news.ycombinator.com/best?p=#{page}"
 
-    data = []
+    Rails.cache.fetch(url, expires_in: 5.days) do
+      data = []
 
-    document = parsed_document("https://news.ycombinator.com/best?p=#{page}")
+      document = parsed_document(url)
 
-    item_list = document.css('.itemlist')
+      item_list = document.css('.itemlist')
 
-    titles = item_list.css('.storylink')
-    titles.each do |title|
-      news = News.new({ title: title.text, url: title.attributes['href'].value })
-      data << news
-    end
-
-    sub_titles = item_list.css('.sitestr')
-    sub_titles.each_with_index do |sub_title, index|
-      data[index].sub_title = sub_title.text
-    end
-
-    descriptions = item_list.css('.subtext')
-    descriptions.each_with_index do |desc, index|
-      data[index].description = desc.text.gsub(/\n/, "").strip
-    end
-
-    cover_image_urls = {}
-    threads = []
-    data.each do |news|
-      threads << Thread.new do
-        document = parsed_document(news.url)
-        og_image = document.at('meta[property="og:image"]')
-
-        image_url = og_image.present? ? og_image.attributes['content'].value : ''
-        cover_image_urls[news.url] = image_url
+      titles = item_list.css('.storylink')
+      titles.each do |title|
+        news = News.new({ title: title.text, url: title.attributes['href'].value })
+        data << news
       end
-    end
-    threads.map(&:join)
 
-    data.each do |news|
-      news.cover_image_url = cover_image_urls[news.url]
-    end
+      sub_titles = item_list.css('.sitestr')
+      sub_titles.each_with_index do |sub_title, index|
+        data[index].sub_title = sub_title.text
+      end
 
-    data
+      descriptions = item_list.css('.subtext')
+      descriptions.each_with_index do |desc, index|
+        data[index].description = desc.text.gsub(/\n/, "").strip
+      end
+
+      cover_image_urls = {}
+      threads = []
+      data.each do |news|
+        threads << Thread.new do
+          document = parsed_document(news.url)
+          og_image = document.at('meta[property="og:image"]')
+
+          image_url = og_image.present? ? og_image.attributes['content'].value : ''
+          cover_image_urls[news.url] = image_url
+        end
+      end
+      threads.map(&:join)
+
+      data.each do |news|
+        news.cover_image_url = cover_image_urls[news.url]
+      end
+
+      data
+    end
   end
 end
--- app/services/single_news_scraper_service.rb
+++ app/services/single_news_scraper_service.rb
@@ -2,20 +2,22 @@ class SingleNewsScraperService
   include ScraperHelpers
 
   def self.execute(url: )
-    source = URI.open(URI.encode(url)).read
+    Rails.cache.fetch(url, expires_in: 5.days) do
+      source = URI.open(URI.encode(url)).read
 
-    document = parsed_document(url)
+      document = parsed_document(url)
 
-    image = document.at('meta[property="og:image"]')
-    title = document.at('meta[property="og:title"]').attributes['content'].value
-    description = document.at('meta[property="og:description"]').attributes['content'].value
+      image = document.at('meta[property="og:image"]')
+      title = document.at('meta[property="og:title"]').attributes['content'].value
+      description = document.at('meta[property="og:description"]').attributes['content'].value
 
-    News.new({
-      url: url,
-      cover_image_url: image.present? ? image.attributes['content'].value : '',
-      title: title,
-      description: description,
-      content: Readability::Document.new(source).content
-    })
+      News.new({
+        url: url,
+        cover_image_url: image.present? ? image.attributes['content'].value : '',
+        title: title,
+        description: description,
+        content: Readability::Document.new(source).content
+      })
+    end
   end
 end
