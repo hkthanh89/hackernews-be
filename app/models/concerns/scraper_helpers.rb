bb705e5837a346150cef1af41ce0eec0997a5186
Thanh Huynh <hkthanh89@users.noreply.github.com>
2021-07-18 16:57:39 +0700

    parse document with utf8 encoding

--- app/models/concerns/scraper_helpers.rb
+++ app/models/concerns/scraper_helpers.rb
@@ -7,7 +7,7 @@ module ScraperHelpers
   class_methods do
     def parsed_document(url)
       html = URI.open(URI.encode(url), ::USER_AGENT)
-      Nokogiri::HTML(html)
+      Nokogiri::HTML(html, nil, Encoding::UTF_8.to_s)
     end
 
     def element_content(element)
--- spec/services/list_news_scraper_service_spec.rb
+++ spec/services/list_news_scraper_service_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe ListNewsScraperService, type: :service do
   describe '.execute' do
     let(:cassette) { 'list_news' }
+    let(:page) { nil }
     let(:subject) do
       VCR.use_cassette(cassette) do
-        described_class.execute(page: nil)
+        described_class.execute(page: page)
       end
     end
 
@@ -32,9 +33,28 @@ RSpec.describe ListNewsScraperService, type: :service do
         cover_image_url: 'https://cdn.cloudflare.steamstatic.com/steamdeck/images/steamdeck_social.jpg',
         title: 'Valve Steam Deck',
         sub_title: 'steamdeck.com',
-        description: '2460 points by homarp 1 day ago  | 1546 comments',
+        description: '2491 points by homarp 2 days ago  | 1581 comments',
         content: nil
       }.as_json)
+    end
+
+    context 'encoding format' do
+      let(:cassette) { 'list_news_page_2' }
+      let(:page) { 2 }
+
+      it 'should parse document as UTF-8' do
+        data = subject
+        news = data[24]
+
+        expect(news.as_json).to eq({
+          url: 'https://www.ana.press/photo/548339/دنیای-مداد-رنگی',
+          cover_image_url: 'https://media.ana.press/old/1399/09/24/637435500045793867_lg.JPG',
+          title: 'Mr. Rafieh’s Tehran Pencil Shop',
+          sub_title: 'ana.press',
+          description: '231 points by NaOH 3 days ago  | 76 comments',
+          content: nil
+        }.as_json)
+      end
     end
 
     context 'caching' do
