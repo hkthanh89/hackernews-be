3e2a3179291b9e2b1b1f70fc5fb0c4f2abe65c8b
Thanh Huynh <hkthanh89@users.noreply.github.com>
2023-10-18 12:53:28 +0700

    filter jobs and models folders from test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,10 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 #
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter "/jobs/"
+  add_filter "/models/"
+end
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
